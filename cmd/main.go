c98eca59f2a434a40c93bd46ec042e8788287974
eaglexiang <eagle.xiang@outlook.com>
2024-12-12 14:32:12 +0800

    兼容 JSON 格式的输入

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -14,10 +18,47 @@ func main() {
 		fmt.Println("source file cannot be empty")
 		return
 	}
+	filename := *src
 
-	cmd := fmt.Sprintf("cat %s | flamegraph.pl > flamegraph.svg", *src)
-	err := exec.Command("bash", "-c", cmd).Run()
+	ext := filepath.Ext(filename)
+
+	var err error
+	switch ext {
+	case ".json":
+		err = jsonDraw(filename)
+	default:
+		err = defaultDraw(filename)
+	}
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func defaultDraw(filename string) (err error) {
+	cmd := fmt.Sprintf("cat %s | flamegraph.pl > flamegraph.svg", filename)
+	err = exec.Command("bash", "-c", cmd).Run()
+	return
+}
+
+func jsonDraw(filename string) (err error) {
+	buf, err := os.ReadFile(filename)
+	if err != nil {
+		return
+	}
+	lines := []string{}
+	err = json.Unmarshal(buf, &lines)
+	if err != nil {
+		return
+	}
+
+	text := strings.Join(lines, "\n")
+	err = os.WriteFile("tmp.log", []byte(text), os.ModePerm)
+	if err != nil {
+		return
+	}
+	defer os.Remove("tmp.log")
+
+	err = defaultDraw("tmp.log")
+
+	return
 }
