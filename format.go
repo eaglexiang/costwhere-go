4d1e0a5c9f593a68e966413dd4cc11aa2854a670
eaglexiang <eagle.xiang@outlook.com>
2024-12-13 16:27:45 +0800

    所有直接子节点的时间都需要扣除

--- format.go
+++ format.go
@@ -19,9 +19,9 @@ func formatCosts(c *Costs) (costs []string) {
 
 	for i := 0; i < len(arr); i++ {
 		for j := i + 1; j < len(arr); j++ {
-			if strings.HasPrefix(arr[j].Path, arr[i].Path) {
+			if strings.HasPrefix(arr[j].Path, arr[i].Path) &&
+				isDirectChild(arr[i].Path, arr[j].Path) {
 				arr[i].Cost -= arr[j].Cost
-				break
 			}
 		}
 	}
@@ -31,5 +31,12 @@ func formatCosts(c *Costs) (costs []string) {
 		costs = append(costs, text)
 	}
 
+	return
+}
+
+// isDirectChild 判断 child 是否是 parent 的直接子节点
+func isDirectChild(parent string, child string) (is bool) {
+	after, _ := strings.CutPrefix(child, parent)
+	is = strings.Count(after, ";") == 1
 	return
 }
